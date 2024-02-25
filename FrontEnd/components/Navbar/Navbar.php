6233b6881d79c8e41b2937c0d97a6d5fcdc1bb44
SahilSuresh <getshahilsuresh38@gmail.com>
2024-02-25 19:20:24 +0000

    Nav bar
    Made the navigation responsive

--- FrontEnd/components/Navbar/Navbar.php
+++ FrontEnd/components/Navbar/Navbar.php
@@ -1,3 +1,22 @@
 <?php require('components/Header/header.php')?>
-<link rel="stylesheet" href="./components/Navbar/Navbar.css">
-<h1>Navbar</h1>
+<link rel="stylesheet" href="/components/Navbar/Navbar.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" >
+  
+
+  <nav>
+    <input type="checkbox" id="check">
+    <label for="check">
+      <i class="fas fa-bars" id="btn"></i>
+      <i class="fas fa-times" id="cancel"></i>
+    </label>
+    
+    <img src= "images/logo.png" alt="logo">
+    <ul class = "divScroll">
+      <li><a href="/">Home</a></li>
+      <li><a href="/contact">Contact</a></li>
+      <li><a href="/threads">Event</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/login">login</a></li>
+      <li><a href="/register">Sign Up</a></li>
+  </ul>
+  </nav>
