440fe80fe5c50e4abbb6407d54f35d3d6c1591db
SahilSuresh <getshahilsuresh38@gmail.com>
2024-02-25 21:09:04 +0000

    changed navbar

--- FrontEnd/components/Navbar/Navbar.php
+++ FrontEnd/components/Navbar/Navbar.php
@@ -1,5 +1,55 @@
 <?php require('components/Header/header.php')?>
-<link rel="stylesheet" href="/components/Navbar/Navbar.css">
+
+<!-- <link rel="stylesheet" type="text/css" href="./components/Navbar/Navbar.css"> -->
+
+<!-- </head>
+<body>
+
+  <link rel="stylesheet" href="/components/Navbar/Navbar.css">
+  </head>
+  <body>
+    
+    <nav class="navbar">
+      <div class="navbar-container">
+        <div class="navbar-image">
+          <img src="./images/logo.png" alt = "logo"> 
+        </div>
+        <div class="nav-links">
+          <a href="/">Home</a></li>
+          <a href="/about">About</a>
+          <a href="/threads">Services</a>
+          <a href="/contact">Contact</a>
+        </div>
+        <div class="navbar-buttons">
+          
+            <a href="/login">Login</a>
+          
+          
+         
+            <a href="/register">Sign Up</a>
+          
+        </div>
+          <div class="navbar-menu">
+            <img id="icon-menu" src="images/icon-menu.png" alt="menu">
+            <img id="icon-close" src="images/icon-close.png" alt="close">
+
+            <div id="menu" class="invisible">
+              <a href="/">Home</a></li>
+              <a href="/contact">Contacts</a></li>
+              <a href="/about">About</a></li>
+            </div>
+              
+          </div>
+      </div>
+    </nav>
+
+    <script src="/components/Navbar/Navbar.js"></script>
+  </body>
+
+</body> -->
+
+
+  <link rel="stylesheet" href="/components/Navbar/Navbar.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" >
   
 
@@ -20,3 +70,7 @@
       <li><a href="/register">Sign Up</a></li>
   </ul>
   </nav>
+
+
+
+  
