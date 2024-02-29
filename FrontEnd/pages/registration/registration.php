db09c3ecfee01c044427ecde83fbe64eeaa49404
wilfried kamdoum <kamdoumwilfried8@gmail.com>
2024-02-29 19:46:55 +0100

    made navbar

--- FrontEnd/pages/registration/registration.php
+++ FrontEnd/pages/registration/registration.php
@@ -2,29 +2,18 @@
     <link rel="stylesheet" href="/style/register">
 </head>
 <?php require("components/Navbar/Navbar.php") ?>
-<body>
-<div class="container">
+
+<main>
     <div class="row">
         <h2>Registration Form</h2>
         <form method="POST" id="registrationForm">
-            <label for="firstname">First Name:</label>
             <input type="text" id="firstname" name="firstname" required>
-            
-            <label for="lastname">Last Name:</label>
             <input type="text" id="lastname" name="lastname" required>
-            
-            <label for="password">Password:</label>
             <input type="password" id="password" name="password" required>
-            
-            <label for="phone">Phone Number:</label>
             <input type="tel" id="phone" name="phone" required>
-            
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-            
+            <input type="email" id="email" name="email" required> 
             <button type="submit">Register</button>
         </form>
     </div>
-</div>
-</body>
+</main>
 <script src="/javascript/registration"></script>
