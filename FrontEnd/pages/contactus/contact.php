1ddb9d4b07869cf8f05265fb2ac5327b05cce82b
Danny <dannypechab@gmail.com>
2024-03-20 01:41:17 +0000

    updating contactus page

--- FrontEnd/pages/contactus/contact.php
+++ FrontEnd/pages/contactus/contact.php
@@ -1,6 +1,88 @@
 <?php require("components/Navbar/Navbar.php") ?>
+<head>
+<Link href="contact.css" rel="stylesheet">
+<link rel="stylesheet"
+href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
 <body>
-    <div>
-        <h1>Contact</h1>
+
+<section class="team">
+<div class="container">
+<h1>OUR TEAM</h1>
+<div class="row">
+<div class="col-md-3 profile text-center">
+<div class="img-box">
+<img src="team1.jpg" class="img-responsive">
+<ul>
+<a href="#"><li><i class="fa fa-facebook"></i></li></a>
+    <a href="#"><li><i class="fa fa-instagram"></i></li></a>
+        <a href="linkdin.com/in/danny-greenfield-pechab-a6510627b/"><li><i class="fa fa-linkedin"></i></li></a>
+</ul>
+</div>
+<h2>Danny Greenfield</h2>
+<h3>Back-End/Front-end</h3>
+<p>Tests all functionalities, and also does some Front-end designs. </p>
+</div>
+
+
+<div class="col-md-3 profile text-center">
+    <div class="img-box">
+    <img src="team2.jpg" class="img-responsive">
+    <ul>
+    <a href="#"><li><i class="fa fa-facebook"></i></li></a>
+        <a href="#"><li><i class="fa fa-instagram"></i></li></a>
+            <a href="#"><li><i class="fa fa-linkedin"></i></li></a>
+    </ul>
     </div>
+    <h2>Wilfred Kamdoum</h2>
+    <h3>Back-End</h3>
+    <p>Uses PHP to connect Front-end designs to the Back-End,testing.</p>
+    </div>
+
+    <div class="col-md-3 profile text-center">
+        <div class="img-box">
+        <img src="team3.jpg" class="img-responsive">
+        <ul>
+        <a href="#"><li><i class="fa fa-facebook"></i></li></a>
+            <a href="#"><li><i class="fa fa-instagram"></i></li></a>
+                <a href="#"><li><i class="fa fa-linkedin"></i></li></a>
+        </ul>
+        </div>
+        <h2>Leonardo</h2>
+        <h3>Back-End</h3>
+        <p>Brings about database by using SQL to create all tables.</p>
+        </div>
+
+        <div class="col-md-3 profile text-center">
+            <div class="img-box">
+            <img src="team4.jpg" class="img-responsive">
+            <ul>
+            <a href="#"><li><i class="fa fa-facebook"></i></li></a>
+                <a href="#"><li><i class="fa fa-instagram"></i></li></a>
+                    <a href="#"><li><i class="fa fa-linkedin"></i></li></a>
+            </ul>
+            </div>
+            <h2>Sahil</h2>
+            <h3>Front-end</h3>
+            <p>Head of Front-end, designs home page, with nav bars, footers.</p>
+            </div>
+            <div class="col-md-3 profile text-center">
+                <div class="img-box">
+                <img src="team5.jpg" class="img-responsive">
+                <ul>
+                <a href="#"><li><i class="fa fa-facebook"></i></li></a>
+                    <a href="#"><li><i class="fa fa-instagram"></i></li></a>
+                        <a href="#"><li><i class="fa fa-linkedin"></i></li></a>
+                </ul>
+                </div>
+                <h2>Andrej</h2>
+                <h3>Front-end</h3>
+                <p>Brings about database by using SQL to create all tables.</p>
+                </div>
+            
+
+</div>
+</div>
+</section>
 </body>
