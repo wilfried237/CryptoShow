dad9b0f349808ce12e28ce496a8500650b094597
SahilSuresh <getshahilsuresh38@gmail.com>
2024-03-26 16:56:38 +0000

    Made home and about us page

--- FrontEnd/pages/pages/aborts/404.php
+++
@@ -1,4 +0,0 @@
-<?php require("components/Navbar/Navbar.php") ?>
-<body>
-    <h1>Page not found 404</h1>
-</body>
--- FrontEnd/pages/pages/home/home.php
+++
@@ -1,6 +0,0 @@
-<?php require("components/Navbar/Navbar.php") ?>
-<body>
-    <div>
-        <h1>Home Page</h1>
-    </div>
-</body>
