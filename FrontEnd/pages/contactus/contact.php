efd2e1325a1f0be9f8989e50eec33d5528cb52bb
Danny <dannypechab@gmail.com>
2024-03-27 09:49:45 +0000

    contactUs page

--- FrontEnd/pages/contactus/contact.php
+++ FrontEnd/pages/contactus/contact.php
@@ -1,321 +1,47 @@
 <?php require("components/Navbar/Navbar.php") ?>
-<<<<<<< HEAD
-<head>
-<Link href="/style/contactus" rel="stylesheet">
-<link rel="stylesheet"
-href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<body>
 
-<section class="team">
-<div class="container">
-<h1>OUR TEAM</h1>
-<div class="row">
-<div class="col-md-3 profile text-center">
-<div class="img-box">
-<img src="team1.jpg" class="img-responsive">
-<ul>
-<a href="#"><li><i class="fa fa-facebook"></i></li></a>
-    <a href="#"><li><i class="fa fa-instagram"></i></li></a>
-        <a href="linkdin.com/in/danny-greenfield-pechab-a6510627b/"><li><i class="fa fa-linkedin"></i></li></a>
-</ul>
-</div>
-<h2>Danny Greenfield</h2>
-<h3>Back-End/Front-end</h3>
-<p>Tests all functionalities, and also does some Front-end designs. </p>
-</div>
-
-
-<div class="col-md-3 profile text-center">
-    <div class="img-box">
-    <img src="team2.jpg" class="img-responsive">
-    <ul>
-    <a href="#"><li><i class="fa fa-facebook"></i></li></a>
-        <a href="#"><li><i class="fa fa-instagram"></i></li></a>
-            <a href="#"><li><i class="fa fa-linkedin"></i></li></a>
-    </ul>
+<html lang="en">
+    <head>
+<meta charset="UTF-8">
+<title>Our Team</title>
+    <link rel="stylesheet" href="/pages/contactus/contact.css">
+    </head>
+    <body>
+        <div class="container">
+        <div class="wrapper"> 
+<h2>OUR TEAM</h2>
+<hr>
+<div class="members">
+    <div class="team-mem">
+        <img src="/images/team1.jpg" alt="">
+        <h3>Mr.Greenfield</h3>
+        <p>Back-End/Front-End</p>
     </div>
-    <h2>Wilfred Kamdoum</h2>
-    <h3>Back-End</h3>
-    <p>Uses PHP to connect Front-end designs to the Back-End,testing.</p>
-    </div>
-
-    <div class="col-md-3 profile text-center">
-        <div class="img-box">
-        <img src="team3.jpg" class="img-responsive">
-        <ul>
-        <a href="#"><li><i class="fa fa-facebook"></i></li></a>
-            <a href="#"><li><i class="fa fa-instagram"></i></li></a>
-                <a href="#"><li><i class="fa fa-linkedin"></i></li></a>
-        </ul>
+    <div class="team-mem">
+        <img src="/images/team2.jpg" alt="">
+        <h3>Mr. Wilfred</h3>
+        <p>Back-End/Front-End</p>
+</div>
+<div class="team-mem">
+    <img src="/images/team3.jpg" alt="">
+    <h3>Mr. Leo</h3>
+    <p>Back-End</p>
+</div>
+<div class="team-mem">
+    <img src="/images/team4.jpg" alt="">
+    <h3>Mr. Sahil</h3>
+    <p>Front-End</p>
+</div>
+<div class="team-mem">
+    <img src="/images/team5.jpg" alt="">
+    <h3>Mr. Andrej</h3>
+    <p>Front-End</p>
+</div>
+</div>
         </div>
-        <h2>Leonardo</h2>
-        <h3>Back-End</h3>
-        <p>Brings about database by using SQL to create all tables.</p>
-        </div>
-
-        <div class="col-md-3 profile text-center">
-            <div class="img-box">
-            <img src="team4.jpg" class="img-responsive">
-            <ul>
-            <a href="#"><li><i class="fa fa-facebook"></i></li></a>
-                <a href="#"><li><i class="fa fa-instagram"></i></li></a>
-                    <a href="#"><li><i class="fa fa-linkedin"></i></li></a>
-            </ul>
-            </div>
-            <h2>Sahil</h2>
-            <h3>Front-end</h3>
-            <p>Head of Front-end, designs home page, with nav bars, footers.</p>
-            </div>
-            <div class="col-md-3 profile text-center">
-                <div class="img-box">
-                <img src="team5.jpg" class="img-responsive">
-                <ul>
-                <a href="#"><li><i class="fa fa-facebook"></i></li></a>
-                    <a href="#"><li><i class="fa fa-instagram"></i></li></a>
-                        <a href="#"><li><i class="fa fa-linkedin"></i></li></a>
-                </ul>
-                </div>
-                <h2>Andrej</h2>
-                <h3>Front-end</h3>
-                <p>Brings about database by using SQL to create all tables.</p>
-                </div>
-            
-
 </div>
+    </body>
+    
 </div>
-</section>
-</body>
-=======
-<link rel="stylesheet" href="/style/contact">
-
-<body>
-  <div class="container">
-    <div class="main">
-      <div class="holder">
-      <h1>Our Vision</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p</p>
-      <img src="https://images.pexels.com/photos/1181273/pexels-photo-1181273.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" id="main-image">
-      <img src="https://images.pexels.com/photos/2837382/pexels-photo-2837382.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" id="main-image1">
-      </div>
-    </div>
-    <div class = "middle">
-     <h1>The Dream Team</h1>
-      <p>lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua lorem lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p</p>
-    </div>
-    <div class="threads-main"><h1> Events </h1>
-      <section class="threads-section">
-
-        <article class="threads-article">
-            <img src="https://www.eventsindustryforum.co.uk/images/articles/about_the_eif.jpg" class="thread-image">
-            <div class="threads-info">
-                <div class="threads-date-location">
-                    <div class="thread-date">
-                        <p class="date">16</p>
-                        <p class="month">SEPT</p>
-                    </div>
-                    <div class="thread-location">
-                        <p class="title">crypto Show</p>
-                        <p class="location">Leicester</p>
-                    </div>
-                </div>
-                <div class="thread-book">
-                    <div class="thread-profile-username">
-                        <a class="threads-userProfile">WK</a>
-                        <p class="thread-username">Wilfried Kamdoum</p>
-                    </div>
-                    <button>Book</button>
-                </div>
-            </div>
-        </article>
-        <article class="threads-article">
-            <img src="https://images.pexels.com/photos/2774556/pexels-photo-2774556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="thread-image">
-            <div class="threads-info">
-                <div class="threads-date-location">
-                    <div class="thread-date">
-                        <p class="date">16</p>
-                        <p class="month">SEPT</p>
-                    </div>
-                    <div class="thread-location">
-                        <p class="title">crypto Show</p>
-                        <p class="location">Leicester</p>
-                    </div>
-                </div>
-
-                <div class="thread-book">
-                <div class="thread-profile-username">
-                    <a class="threads-userProfile">WK</a>
-                    <p class="thread-username">Wilfried Kamdoum</p>
-                </div>
-                <button>Book</button>
-                </div>
-            </div>
-        </article>
-        <article class="threads-article">
-            <img src="https://images.pexels.com/photos/1983046/pexels-photo-1983046.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="thread-image">
-            <div class="threads-info">
-                <div class="threads-date-location">
-                    <div class="thread-date">
-                        <p class="date">16</p>
-                        <p class="month">SEPT</p>
-                    </div>
-                    <div class="thread-location">
-                        <p class="title">crypto Show</p>
-                        <p class="location">Leicester</p>
-                    </div>
-                </div>
-
-                <div class="thread-book">
-                <div class="thread-profile-username">
-                    <a class="threads-userProfile">WK</a>
-                    <p class="thread-username">Wilfried Kamdoum</p>
-                </div>
-                <button>Book</button>
-          <!-- <button id="see-more">See More</button> -->
-                </div>
-            </div>
-        </article>
-        <article class="threads-article">
-            <img src="https://images.pexels.com/photos/1587927/pexels-photo-1587927.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="thread-image">
-            <div class="threads-info">
-                <div class="threads-date-location">
-                    <div class="thread-date">
-                        <p class="date">16</p>
-                        <p class="month">SEPT</p>
-                    </div>
-                    <div class="thread-location">
-                        <p class="title">crypto Show</p>
-                        <p class="location">Leicester</p>
-                    </div>
-                </div>
-
-                <div class="thread-book">
-                <div class="thread-profile-username">
-                    <a class="threads-userProfile">WK</a>
-                    <p class="thread-username">Wilfried Kamdoum</p>
-                </div>
-                <button>Book</button>
-          <!-- <button id="see-more">See More</button> -->
-                </div>
-            </div>
-        </article>
-      </section>
-      <section class="threads-section" id="two">
-
-        <article class="threads-article">
-            <img src="https://images.pexels.com/photos/2952834/pexels-photo-2952834.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="thread-image">>
-            <div class="threads-info">
-                <div class="threads-date-location">
-                    <div class="thread-date">
-                        <p class="date">16</p>
-                        <p class="month">SEPT</p>
-                    </div>
-                    <div class="thread-location">
-                        <p class="title">crypto Show</p>
-                        <p class="location">Leicester</p>
-                    </div>
-                </div>
-
-                <div class="thread-book">
-                <div class="thread-profile-username">
-                    <a class="threads-userProfile">WK</a>
-                    <p class="thread-username">Wilfried Kamdoum</p>
-                </div>
-                <button>Book</button>
-                </div>
-            </div>
-        </article>
-        <article class="threads-article">
-            <img src="https://images.pexels.com/photos/1190297/pexels-photo-1190297.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="thread-image">
-            <div class="threads-info">
-                <div class="threads-date-location">
-                    <div class="thread-date">
-                        <p class="date">16</p>
-                        <p class="month">SEPT</p>
-                    </div>
-                    <div class="thread-location">
-                        <p class="title">crypto Show</p>
-                        <p class="location">Leicester</p>
-                    </div>
-                </div>
-
-                <div class="thread-book">
-                <div class="thread-profile-username">
-                    <a class="threads-userProfile">WK</a>
-                    <p class="thread-username">Wilfried Kamdoum</p>
-                </div>
-                <button>Book</button>
-                </div>
-            </div>
-        </article>
-        <article class="threads-article">
-            <img src="https://images.pexels.com/photos/2774556/pexels-photo-2774556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="thread-image">
-            <div class="threads-info">
-                <div class="threads-date-location">
-                    <div class="thread-date">
-                        <p class="date">16</p>
-                        <p class="month">SEPT</p>
-                    </div>
-                    <div class="thread-location">
-                        <p class="title">crypto Show</p>
-                        <p class="location">Leicester</p>
-                    </div>
-                </div>
-
-                <div class="thread-book">
-                <div class="thread-profile-username">
-                    <a class="threads-userProfile">WK</a>
-                    <p class="thread-username">Wilfried Kamdoum</p>
-                </div>
-                <button>Book</button>
-                </div>
-            </div>
-        </article>
-        <article class="threads-article">
-            <img src="https://images.pexels.com/photos/2747446/pexels-photo-2747446.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="thread-image">
-            <div class="threads-info">
-              <h1>Andrej</h1>
-                <!-- <div class="threads-date-location">
-                    <div class="thread-date">
-                        <p class="date">16</p>
-                        <p class="month">SEPT</p>
-                    </div>
-                    <div class="thread-location">
-                        <p class="title">crypto Show</p>
-                        <p class="location">Leicester</p>
-                    </div>
-                </div> -->
-
-                <div class="thread-book">
-                <div class="thread-profile-username">
-                    <a class="threads-userProfile">WK</a>
-                    <p class="thread-username">Wilfried Kamdoum</p>
-                </div>
-                <button>Book</button>
-                </div>
-            </div>
-        </article>
-      </section>
-    </div>
-  <div class="contact-container">
-    <div class="contact-header">
-      <h1>Have Some Questions?</h1>
-    </div>
-    <div class="both">
-    <img src="https://images.pexels.com/photos/18536259/pexels-photo-18536259/free-photo-of-the-word-contact-is-spelled-out-with-wooden-letters.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" id="hero-image">
-    <div class = "contact-form">
-      <form method="POST" id="contactForm">
-        <input placeholder="First Name" type="text" id="firstname" name="firstname" required>
-        <input placeholder="Last Name" type="text" id="lastname" name="lastname" required>
-        <input placeholder="Email" type="email" id="email" name="email" required>
-        <input placeholder="Message" type="text" id="message" name="message" required>
-        <button>Contact Us</button>
-    </div>
-    </div>
-  </div>
-  </div>
-</body>
-
 <?php require("components/Footer/Footer.php") ?>
->>>>>>> origin/Sahil
+</html>
