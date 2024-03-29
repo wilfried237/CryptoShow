636a132fe549290571d317be54cc6e83f5afacf7
SahilSuresh <getshahilsuresh38@gmail.com>
2024-03-29 11:21:23 +0000

    Added admin_event page with the architecture

--- FrontEnd/index.php
+++ FrontEnd/index.php
@@ -10,8 +10,6 @@
         '/threads'=>'./pages/threads/threads.php',
         '/login'=>'./pages/login/login.php',
         '/register'=> './pages/registration/registration.php',
-        '/admin' => './pages/admin/admin.php'
-        '/memberAdmin' => './pages/admin_member/admin.php'
         '/personalAccount'=> './pages/personalAccount/personal.php',
         '/threadInfo'=> './pages/threadInfo/threadInfo.php',
         '/CreateEvent'=> './pages/OrganiserThread/createEvent.php',
@@ -19,6 +17,9 @@
         '/style'=>'./routes/style.routes.php',
         '/images'=> './routes/images.routes.php',
         '/javascript'=> './routes/javascript.routes.php',
+        '/admin' => './pages/admin/admin.php',
+        '/memberAdmin' => './pages/admin_member/member.php',
+        '/adminEvent' => './pages/admin_event/event.php',
     ];
 
     routes_to_Controller_front($uri,$routes);
